83ca9245b6d4352331f213805006563ed06f268c
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-05-19 23:50:06 +0900

    v-model을 이용하여 간편 구현

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,47 @@
 <template>
-  
+  <div class="container">
+    <h1>TodoList</h1>
+    <!--form 태그-->
+    <form @submit.prevent="Submit" class="d-flex">
+      <!--할일 추가-->
+      <div class="flex-grow-1">
+        <input class="form-control" type="text" v-model="todo" placeholder="할 일을 적어주세요!">
+      </div>
+      <!--버튼-->
+      <div>
+        <button class="btn btn-primary" @click="Submit">추가하기</button>
+      </div>
+    </form>
+
+    <!--리스트를 표시함-->
+    {{ todolist }}
+  </div>
 </template>
 
 <script>
+import {ref} from 'vue'
 
 export default {
-  
+  setup(){
+    let todo = ref(""); // 할 일 입력 값을 저장
+    let todolist = ref([]); // 할 일 목록을 저장
+
+    // 할 일을 추가
+    var Submit = () => {
+      todolist.value.push({
+        id: Date.now(), // 현재 시간 사용
+        sbj: todo.value // 할 일 내용
+      });
+
+      todo.value = ""; // 입력 필드 초기화 (안하면 두번 나옴)
+    }
+
+    return {
+      todo,Submit,todolist,
+    };
+  }
 }
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { BootstrapVue3 } from 'bootstrap-vue-3'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(BootstrapVue3)
+app.mount('#app')
